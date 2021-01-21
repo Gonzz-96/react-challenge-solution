fe6201fa61105266f1fb4747aa414421b0f049ff
Gonzalo Campos <54908614+Gonzz-96@users.noreply.github.com>
2021-01-21 12:02:50 -0600

    Remove random comment.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./styles.css";
 import PokeCard from "./components/PokeCard";
 import { mockPokemonData } from "./mock/pokeData";
 
-// hello, this is a comment
 export default function App() {
   return (
     <div className="App">
