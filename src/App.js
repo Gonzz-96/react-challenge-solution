a3b01b8841b24a73188aecec916a31d9149b528d
Gonzalo Campos <54908614+Gonzz-96@users.noreply.github.com>
2021-01-21 11:31:43 -0600

    Create PokeCard component and render it in App component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./styles.css";
+import PokeCard from "./components/PokeCard";
 
 // hello, this is a comment
 export default function App() {
   return (
     <div className="App">
-      <h1>Welcome to your second mini challenge!</h1>
+      <PokeCard />
     </div>
   );
 }
---
+++ src/components/PokeCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PokeCard = () => {
+  return (
+    <>
+      <h1>PokeCard</h1>
+    </>
+  );
+};
+
+export default PokeCard;
